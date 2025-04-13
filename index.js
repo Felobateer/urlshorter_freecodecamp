b92fff28678e4548fff31ebc9245a9b1164d437a
Felobateer <felobateersamy01@gmail.com>
2025-04-13 08:26:17 +0200

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-
+const url_db = {};
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -22,11 +22,39 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+function isValidHttpUrl(string) {
+  try {
+    const url = new URL(string);
+    return url.protocol === 'http:' || url.protocol === 'https:';
+  } catch(_) {
+    return false;
+  }
+}
+
 app.post('/api/shorturl', function(req, res) {
   const url = req.body.url;
-  const shortUrl = Math.floor(Math.random() * 10);
+
+  if(!isValidHttpUrl(url)) {
+    return res.status(400).json({ error: 'invalid url' });
+  }
+
+  const shortUrl = Math.floor(Math.random() * 10).toString();
+
+  url_db[shortUrl] = url;
+
   res.json({ original_url: url, short_url: shortUrl });
 });
+
+app.get('/api/shorturl/:short_url', function(req, res) {
+  const short = req.params.short_url;
+  const url = url_db[short];
+
+  if(url) {
+    res.redirect(url);
+  } else {
+    res.status(404).json({ error: 'invalid url' });
+  }
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
