0ea6f94dbf8bbf571404aca27640e0b2c7fab45a
Felobateer <felobateersamy01@gmail.com>
2025-04-13 08:10:44 +0200

    fix

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
